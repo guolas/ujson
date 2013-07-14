8214c0a0c78dd6aaaace5a15fce10184590ba7f2
guolas <guolas@gmail.com>
2013-07-15 01:45:38 +0200

    Not yet working... but getting there

--- lib/ujson.rb
+++ lib/ujson.rb
@@ -12,95 +12,205 @@ class Parser
   end
 
   def parse
-      input_enum = @raw_json.each_char
-      loop do
-        current_char = input_enum.next
-        if current_char.eql? '{'
-          object = parse_object(input_enum)
-        end
-        raise StopIteration
-      end
-  end
-
-  def parse_object(input_enum)
-    members = {}
-    string = nil
-    value = nil
-    name_separator = nil
+    input_enum = @raw_json.each_char
+    object = nil
     loop do
       current_char = input_enum.next
       if %r{\S}.match(current_char)
-        if !string
-          if current_char.eql? '"'
-            string = parse_string(input_enum)
-          elsif current_char.eql? ',' && members.size == 0
+        if current_char.eql? '{'
+          object = parse_object(input_enum)
+        elsif current_char.eql? '['
+          object = parse_array(input_enum)
+        else
+          raise FormatError
+        end
+        raise StopIteration
+      end
+    end
+    return object
+  end
+
+  def parse_object(input_enum)
+    object = {}
+    string = nil
+    value = nil
+    output_options = {}
+    current_char = ''
+
+    waiting_for_string = true
+
+    current_char = input_enum.next
+    loop do
+      if %r{\S}.match(current_char)
+        if waiting_for_string
+          if current_char.eql? '}'
+            puts "[parse_object] (2) object=" + object.to_s + ";"
+            raise StopIteration
+          elsif current_char.eql? '"'
+            string = parse_string(input_enum, :first_char => current_char)
+            puts "[parse_object] string=" + string + ";"
+            waiting_for_string = false
+          elsif current_char.eql? ','
+            raise FormatError if object.size == 0
+          else
+            puts "[parse_object] current_char="+current_char+";"
             raise FormatError
           end
-        elsif !name_separator
-          if %r{\S}.match(current_char)
-            value = parse_value(input_enum)
-            members[string] = value
-            string = nil
-            value = nil
-          end
         else
-          raise FormatError unless current_char.eql? ':'
-          name_separator = current_char
+          if current_char.eql? ':'
+            value, output_options = parse_value(input_enum)
+            waiting_for_string = true
+
+            object[string] = value
+
+            puts "[parse_object] object=" + object.to_s + ";"
+            puts "[parse_object] output_options=" + output_options.to_s + ";"
+          else
+            raise FormatError
+          end
         end
       end
-      raise StopIteration if current_char.eql? '}'
+
+      last_char = output_options.fetch(:last_char, '')
+      puts "[parse_object] last_char=" + last_char + ";"
+      if last_char.eql? ''
+        current_char = input_enum.next
+        output_options = {}
+      else
+        current_char = last_char
+        output_options = {}
+      end
     end
 
-    return members
+    return object
+  end
+
+  def parse_array(input_enum)
+    array = []
+    output_options = {}
+
+    puts "vvvvvvvv"    
+    value, outoput_options = parse_value(input_enum)
+    puts "[parse_array] Value = " + value.to_s
+    array << value
+
+    last_char = output_options.fetch(:last_char, '')
+    puts "[parse_array] last_char=" + last_char + ";"
+    if last_char.eql? ''
+      current_char = input_enum.next
+    else
+      current_char = last_char
+    end
+
+    puts "[parse_array] current_char=" + current_char + ";"
+    loop do
+      if %r{\S}.match(current_char)
+        if current_char.eql? ']'
+          raise StopIteration
+        elsif current_char.eql? ','
+          value, output_options = parse_value(input_enum)
+          puts "[parse_array] Value = " + value.to_s
+          array << value
+        else
+          puts "[parse_array] current_char=" + current_char + ";"
+          raise FormatError
+        end
+      end
+      last_char = output_options.fetch(:last_char, '')
+      if last_char.eql? ''
+        current_char = input_enum.next
+        output_options = {}
+      else
+        current_char = last_char
+        output_options = {}
+      end
+    end
+
+    puts "[parse_array] array =" + array.to_s + ";"
+    return array
   end
 
   def parse_value(input_enum)
     value = nil
+    output_options = {}
     loop do
       current_char = input_enum.next
       if %r{\S}.match(current_char)
         if current_char.eql? '"'
-          value = parse_string(input_enum)
+          value = parse_string(input_enum, :first_char => current_char)
         elsif %r{\d}.match(current_char) || current_char.eql?('-')
-          value = parse_number(current_char, input_enum)
+          value, output_options = parse_number(input_enum, current_char)
         elsif current_char.eql? '{'
           value = parse_object(input_enum)
         elsif current_char.eql? '['
           value = parse_array(input_enum)
         elsif current_char.eql? 't'
-          value = parse_literal(current_char, input_enum, 'true')
+          value, output_options = parse_literal(input_enum, current_char, 'true')
         elsif current_char.eql? 'f'
-          value = parse_literal(current_char, input_enum, 'false')
+          value, output_options = parse_literal(input_enum, current_char, 'false')
         elsif current_char.eql? 'n'
-          value = parse_literal(current_char, input_enum, 'null')
-        elsif current_char.eql? ','
+          value, output_options = parse_literal(input_enum, current_char, 'null')
+        else
           raise FormatError unless value
-          raise StopIteration
         end
+        raise StopIteration
       end
     end
-    return value
+
+    last_char = output_options.fetch(:last_char, '')
+    if last_char.eql? ''
+      output_options = {}
+    else
+      output_options = {:last_char => last_char}
+    end
+    return value, output_options
   end
 
-  def parse_string(input_enum)
-    string = ""
-    
-    not_escaped = true
+  def parse_string(input_enum, options ={})
+    string = ''
+
+    escape = false
+    quotation = false
+
+    first_char = options.fetch(:first_char, ' ')
+    if %r{\S}.match(first_char)
+      raise FormatError unless first_char.eql? '"'
+      quotation = true
+    end
 
     loop do
       current_char = input_enum.next
-      raise StopIteration if not_escaped && current_char.eql?('"')
-      string << current_char
-      if current_char.eql? '\\'
-        not_escaped = false
+      if quotation
+        if escape
+          string << curent_char
+          escape = false
+        else
+          if current_char.eql? '\\'
+            string << current_char
+            escape = true
+          elsif current_char.eql? '"'
+            raise StopIteration
+          else
+            string << current_char
+          end
+        end
+      else
+        if %r{\S}.match(current_char)
+          if current_char.eql? '"'
+            quotation = true
+          else
+            raise FormatError
+          end
+        end
       end
     end
     return string
   end
 
-  def parse_number(first_element, input_enum)
+  def parse_number(input_enum, first_element)
+    current_char = ''
     number = first_element 
-    
+
     int = %r{\d}.match(first_element)
     frac = false
     exp = false
@@ -142,38 +252,10 @@ class Parser
         end
       end
     end
-    return number
+    return number, :last_char => current_char
   end
 
-  def parse_array(input_enum)
-    print "\nProcessing Array\n"
-    array = []
-    value = parse_value(input_enum)
-    print "\nRecovered value >", value, "<\n"
-    array << value
-    loop do
-
-      current_char = input_enum.next
-      print current_char
-      
-      raise StopIteration if current_char.eql? ']'
-
-      if %r{\S}.match(current_char)
-        if current_char.eql? ','
-          raise FormatError if array.size == 0
-        else
-          print "\nProcessing A value\n"
-          value = parse_value(input_enum) 
-          print "\n of value >", value, "<\n"
-          array << value
-        end
-      end
-    end
-    print "\nReturning array >", array, "<\n"
-    return array
-  end
-
-  def parse_literal(first_element, input_enum, literal)
+  def parse_literal(input_enum, first_element, literal)
     value = first_element
     template = literal.each_char
     template.next
@@ -184,7 +266,7 @@ class Parser
       value << current_char
     end
 
-    return value
+    return value, {}
   end
 end
 
--- test/individual_test.rb
+++ test/individual_test.rb
@@ -6,7 +6,8 @@ class UJSONTest < MiniTest::Test
   def test_getting_member_with_numeric_value_1
     file = File.new('test/fixtures/member_number_1.json')
     parser = Parser.new(file)
-    member = parser.parse_object(file.each_char)
+    member = parser.parse
+    assert_equal Hash, member.class, 'It should return a Hash'
     assert_equal "hola", member.keys[0], 'It should have only one key with name "hola"'
     assert_equal "-13.303e+3", member["hola"], 'The value stored in "hola" should be "-13.303e+3"'
   end
@@ -14,7 +15,8 @@ class UJSONTest < MiniTest::Test
   def test_getting_member_with_numeric_value_2
     file = File.new('test/fixtures/member_number_2.json')
     parser = Parser.new(file)
-    member = parser.parse_object(file.each_char)
+    member = parser.parse
+    assert_equal Hash, member.class, 'It should return a Hash'
     assert_equal "hola", member.keys[0], 'It should have only one key with name "hola"'
     assert_equal "1.9e-3", member["hola"], 'The value stored in "hola" is not correct'
   end
@@ -22,7 +24,8 @@ class UJSONTest < MiniTest::Test
   def test_getting_member_with_string_value
     file = File.new('test/fixtures/member_string.json')
     parser = Parser.new(file)
-    member = parser.parse_object(file.each_char)
+    member = parser.parse
+    assert_equal Hash, member.class, 'It should return a Hash'
     assert_equal "hola", member.keys[0], 'It should have only one key with name "hola"'
     assert_equal "yoyo", member["hola"], 'The value stored in "hola" is not correct'
   end
@@ -30,7 +33,8 @@ class UJSONTest < MiniTest::Test
   def test_getting_member_with_true_value
     file = File.new('test/fixtures/member_true.json')
     parser = Parser.new(file)
-    member = parser.parse_object(file.each_char)
+    member = parser.parse
+    assert_equal Hash, member.class, 'It should return a Hash'
     assert_equal "hola", member.keys[0], 'It should have only one key with name "hola"'
     assert_equal "true", member["hola"], 'The value stored in "hola" is not correct'
   end
@@ -38,7 +42,8 @@ class UJSONTest < MiniTest::Test
   def test_getting_member_with_false_value
     file = File.new('test/fixtures/member_false.json')
     parser = Parser.new(file)
-    member = parser.parse_object(file.each_char)
+    member = parser.parse
+    assert_equal Hash, member.class, 'It should return a Hash'
     assert_equal "hola", member.keys[0], 'It should have only one key with name "hola"'
     assert_equal "false", member["hola"], 'The value stored in "hola" is not correct'
   end
@@ -46,17 +51,59 @@ class UJSONTest < MiniTest::Test
   def test_getting_member_with_null_value
     file = File.new('test/fixtures/member_null.json')
     parser = Parser.new(file)
-    member = parser.parse_object(file.each_char)
+    member = parser.parse
+    assert_equal Hash, member.class, 'It should return a Hash'
     assert_equal "hola", member.keys[0], 'It should have only one key with name "hola"'
     assert_equal "null", member["hola"], 'The value stored in "hola" is not correct'
+  end
+
+  def test_getting_member_with_object_value_1
+    file = File.new('test/fixtures/member_object_1.json')
+    parser = Parser.new(file)
+    member = parser.parse
+    assert_equal Hash, member.class, 'It should return a Hash'
+    assert_equal "hola", member.keys[0], 'It should have only one key with name "hola"'
+    assert_equal Hash, member["hola"].class, 'It should contain a Hash' 
+  end
+
+  def test_getting_member_with_object_value_2
+    file = File.new('test/fixtures/member_object_2.json')
+    parser = Parser.new(file)
+    member = parser.parse
+    assert_equal Hash, member.class, 'It should return a Hash'
+    assert_equal "hola", member.keys[0], 'It should have a first key with name "hola"'
+    assert_equal "adios", member.keys[1], 'It should have a second key with name "adios"'
+    assert_equal Hash, member["hola"].class, 'It should contain a Hash'
+  end
+
+  def test_getting_array_1
+    file = File.new('test/fixtures/array_1.json')
+    parser = Parser.new(file)
+    array = parser.parse
+    assert_equal Array, array.class, 'It should return an Array'
+    assert_equal 2, array.size, 'It should have two elements'
+    assert_equal Hash, array[0].class, 'First element of the array should be a Hash'
+    assert_equal Hash, array[1].class, 'Second element of the array should be a Hash'
+  end
+
+  def test_getting_array_2
+    file = File.new('test/fixtures/array_2.json')
+    parser = Parser.new(file)
+    array = parser.parse
+    assert_equal Array, array.class, 'It should return an Array'
+    assert_equal 2, array.size, 'It should have two elements'
+    assert_equal Hash, array[0].class, 'First element of the array should be a Hash'
+    assert_equal Hash, array[1].class, 'Second element of the array should be a Hash'
   end
 
   def test_getting_member_with_array_value
     file = File.new('test/fixtures/member_array.json')
     parser = Parser.new(file)
-    member = parser.parse_object(file.each_char)
+    member = parser.parse
+    assert_equal Hash, member.class, 'It should return a Hash'
     assert_equal "hola", member.keys[0], 'It should have only one key with name "hola"'
     assert_equal 5, member["hola"].size, 'The size of the array stored in "hola" should be 5' 
   end
+
 
 end
